17339b7a1824810bb913e66b6eaee3dfe97515f3
Blaine Lewis <blaine.lewis.1@outlook.com>
2024-02-28 12:42:41 -0500

    hid controls in a nicer way.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   useTime,
   useMotionValueEvent,
 } from "framer-motion";
+import { ArrowLeftIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
 
 const mappings = {
   in: {
@@ -75,10 +76,6 @@ function parseSequence(str: string): Sequence[] {
 const fourSevenEight = "in 4 hold 7 out 8";
 
 function App() {
-  const controls =
-    JSON.parse(
-      new URL(window.location.href).searchParams.get("controls") ?? "true",
-    ) ?? true;
   const darkMode =
     JSON.parse(
       new URL(window.location.href).searchParams.get("dark") ?? "true",
@@ -89,6 +86,7 @@ function App() {
       fourSevenEight,
   );
   const [isPlaying, setIsPlaying] = useState(false);
+  const [controlsOpen, setControlsOpen] = useState(false);
 
   let isValidSequence = true;
   try {
@@ -99,49 +97,56 @@ function App() {
 
   return (
     <div
-      className={`w-screen h-screen dark:bg-zinc-900 ${darkMode ? "dark" : ""}`}
+      className={`h-screen w-screen dark:bg-zinc-900 ${darkMode ? "dark" : ""}`}
     >
-      {controls ? (
+      <motion.div
+        className={`absolute left-1/2 -translate-x-1/2`}
+        animate={{
+          top: isPlaying ? "2em" : "50%",
+          opacity: isPlaying ? 0.1 : 1,
+        }}
+      >
         <motion.div
-          className={`fixed top-2 left-1/2 -translate-x-1/2 text-center text-lg`}
-          animate={{ opacity: isPlaying ? 0.1 : 1 }}
+          // layout="position"
+          className="flex items-center divide-x-2 divide-emerald-200 overflow-hidden rounded bg-emerald-100 text-lg text-emerald-700 dark:divide-zinc-700 dark:bg-zinc-800 dark:text-yellow-200"
         >
-          <input
-            type="text"
-            value={sequence}
-            disabled={isPlaying}
-            onChange={(e) => {
-              setSequence(e.target.value);
-            }}
-            className="px-2 py-1 rounded-l bg-emerald-100 text-emerald-700 dark:text-gray-300 dark:bg-zinc-800"
-          />
+          {!isPlaying ? (
+            controlsOpen ? (
+              <motion.input
+                className="min-w-24 bg-transparent px-2 py-1 text-emerald-700 dark:text-gray-300"
+                type="text"
+                value={sequence}
+                disabled={isPlaying}
+                onChange={(e) => {
+                  setSequence(e.target.value);
+                }}
+              />
+            ) : (
+              <button
+                className="flex items-center self-stretch px-2 py-1 hover:bg-emerald-200 hover:dark:bg-zinc-700"
+                onClick={() => setControlsOpen(true)}
+              >
+                <ChevronLeftIcon className="h-5 w-full" />
+              </button>
+            )
+          ) : null}
           <button
             onClick={() => {
               setIsPlaying((a) => !a);
+              setControlsOpen(false);
               const url = new URL(window.location.href);
               url.searchParams.set("sequence", sequence);
               window.history.pushState({}, "", url.toString());
             }}
             disabled={!isValidSequence && !isPlaying}
-            className="px-2 py-1 border-l-2 rounded-r text-emerald-700 hover:bg-emerald-200 dark:text-yellow-200 dark:bg-zinc-800 bg-emerald-100 hover:dark:bg-zinc-700 dark:border-zinc-700 border-emerald-200"
+            className={`px-2 py-1 hover:bg-emerald-200 hover:dark:bg-zinc-700`}
           >
             {isPlaying ? "Stop" : "Begin"}
           </button>
         </motion.div>
-      ) : null}
+      </motion.div>
 
-      {isPlaying ? (
-        <Animation sequence={parseSequence(sequence)} />
-      ) : !controls ? (
-        <button
-          onClick={() => {
-            setIsPlaying((a) => !a);
-          }}
-          className="fixed px-2 py-1 text-lg -translate-x-1/2 -translate-y-1/2 rounded dark:text-yellow-200 dark:bg-zinc-800 hover:dark:bg-zinc-700 dark:border-zinc-700 left-1/2 top-1/2 text-emerald-700 hover:bg-emerald-200 bg-emerald-100"
-        >
-          Begin
-        </button>
-      ) : null}
+      {isPlaying ? <Animation sequence={parseSequence(sequence)} /> : null}
     </div>
   );
 }
@@ -186,9 +191,9 @@ const Animation: React.FC<{ sequence: Sequence[] }> = ({ sequence }) => {
   const padding = 5;
 
   return (
-    <div className="w-full h-full">
+    <div className="h-full w-full">
       <motion.svg
-        className="w-full h-full max-w-xl mx-auto max-h-xl"
+        className="max-h-xl mx-auto h-full w-full max-w-xl"
         viewBox={`-${radius * maxScale + padding} -${radius * maxScale + padding} ${2 * (radius * maxScale + padding)} ${2 * (radius * maxScale + padding)}`}
       >
         <motion.g
@@ -214,6 +219,7 @@ const Animation: React.FC<{ sequence: Sequence[] }> = ({ sequence }) => {
                     pathLength: {
                       type: "spring",
                       duration: 0.5,
+                      // TODO: these delays don't work as well with bigger things... instead I should make it a portion of the total time I want to dedicate to delaying opening.
                       delay: index * 0.15,
                     },
                     opacity: { duration: 0.01 },
@@ -239,7 +245,7 @@ const Animation: React.FC<{ sequence: Sequence[] }> = ({ sequence }) => {
             cx={Math.cos(Math.PI / 50 - 2.3 / radius) * radius}
             cy={Math.sin(Math.PI / 50 - 2.3 / radius) * radius}
             r={2.3}
-            className={`dark:fill-zinc-900 fill-emerald-600 stroke-none opacity-90`}
+            className={`fill-emerald-600 stroke-none opacity-90 dark:fill-zinc-900`}
           />
         </motion.g>
 
@@ -247,7 +253,7 @@ const Animation: React.FC<{ sequence: Sequence[] }> = ({ sequence }) => {
           dominantBaseline="middle"
           textAnchor="middle"
           alignmentBaseline="middle"
-          className={`dark:fill-zinc-200 fill-emerald-700`}
+          className={`fill-emerald-700 dark:fill-zinc-200`}
         >
           {filled.displayText}
         </motion.text>
@@ -256,7 +262,7 @@ const Animation: React.FC<{ sequence: Sequence[] }> = ({ sequence }) => {
           dominantBaseline="middle"
           textAnchor="middle"
           alignmentBaseline="middle"
-          className={`dark:fill-zinc-200 fill-emerald-700`}
+          className={`fill-emerald-700 dark:fill-zinc-200`}
           fontSize={5}
         >
           {Math.floor(Math.floor(time.get()) / (totalDuration * 1000))}
